4773e770c8d6787826cadb588126a187895c9150
udula-herath <herathudula816@gmail.com>
2026-07-20 19:00:07 +0530

    Add files via upload

---
+++ age_calculator.py
@@ -0,0 +1,21 @@
+name=input("What is your name?")
+birth_year=input("enter your birt year")
+age=2026-int(birth_year)
+
+print("hello"+ name +"!")
+print("You are"+ str(age) + "years old in 2026")
+
+print("Welome To Udula supermarket")
+
+item_name=input("\nEnter item name:")
+price=float(input("Enter price of one item(Rs):"))
+quantity=input("enter quantity:")
+total_cost=float(price)*int(quantity)
+
+print("\n_____RCEIPT______")
+print("item_name        :"+item_name)
+print("price            :"+"Rs"+str(price))
+print("Totale Bill      :"+"Rs"+str(total_cost))
+
+print("press enter to exit...")
+      
