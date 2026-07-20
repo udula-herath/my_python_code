3fe0e780b33c2aac406dfe763d4661acb0b5f0be
udula-herath <herathudula816@gmail.com>
2026-07-20 17:03:13 +0530

    Add files via upload

---
+++ simple_variables_exercise.py
@@ -0,0 +1,14 @@
+name ="Udula_Herath"
+age=21
+height=5.7
+is_student=True
+
+
+print("My name is:",name)
+print("My age is:",age)
+print("My height is:",height)
+print("Am I a student?:",is_student)
+
+
+
+input("press enter to exit...")
