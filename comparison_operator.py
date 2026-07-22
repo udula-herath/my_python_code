bf65b04203794ad42af9bd6b3600d0bcd37a131c
udula-herath <herathudula816@gmail.com>
2026-07-22 08:44:02 +0530

    Add files via upload

---
+++ comparison_operator.py
@@ -0,0 +1,25 @@
+print("1.Equal (==)")
+a=10
+B=20.
+print("Is a equal b:",10==20)
+print("Is a equal a:",10==10)
+
+print(" 2.Not Equal (!=)")
+print("Is a not equal b:",10!=20)
+print("Is b not equal b:",20!=20)
+
+print(" 3.Greater Than(>)")
+print("a greater than b:",20>10)
+print("b greater than a:",10>20)
+
+print(" 4.Less Than(<)")
+print("a less than b:",20<10)
+print("b less than a:",10<20)
+
+print(" 5.Greater Than Or Equal(>=)")
+print("Is a greater than or equal b?:",20>=10)
+print("Is a greater than or equal a?:",10>=10)
+
+print(" 6.Less Than or Equal(<=)")
+print("Is a less than or equal b?:",20<=10)
+print("is b less than or equal b?:",20<=20)
