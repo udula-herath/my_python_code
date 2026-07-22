fb9b2b28acd7e181ae8ad486fe98ae33367e387d
udula-herath <herathudula816@gmail.com>
2026-07-22 11:43:59 +0530

    Add files via upload

---
+++ logical_operator.py
@@ -0,0 +1,25 @@
+print("_____PYTHON LOGICAL OPERATOR PRACTICE______")
+print("1.And OPERATOR")
+
+passed_theory=True
+passed_practical=False
+Is_exam_passed=passed_theory and passed_practical
+print("is theory passed?:",passed_theory)
+print("is practical passed?:",passed_practical)
+print("Has student passed thr overall exam?:",Is_exam_passed)
+
+print("2.OR OPERATOR")
+
+has_credit_card=False
+has_cash=True
+is_payment_posible=has_credit_card or has_cash
+print("Has credit card?:",has_credit_card)
+print("Has cash?:",has_cash)
+print("Can the customr pay?:", is_payment_posible)
+
+print("3.NOT OPERATOR")
+
+is_airplane_mode_on=True
+is_network_available=not is_airplane_mode_on
+print("Is airplane mode is on?:",is_airplane_mode_on)
+print("Is mobile network is avilabl?:",is_network_available)
